90cc4c54a6d6f6be5ad735e398cdab1d9c122103
Thanawat-Pete <petelov3dinosaurth@gmail.com>
2026-07-07 17:16:15 +0700

    chore: update Playwright configuration to disable slowMo in CI and remove obsolete trace comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,12 +28,11 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'on',
     video: 'on',
     launchOptions: {
-      slowMo: 1000,
+      slowMo: process.env.CI ? 0 : 1000,
     }
   },
 
